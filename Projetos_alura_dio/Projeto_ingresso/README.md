4398e832929c5bd37b94338810b3d19f03f10bd6
ericsonnascimento <ericson@grupocyber.com.br>
2024-11-24 17:34:55 -0300

    conclusão do projeto ingresso e conclusão do projeto amigo secreto restando apenas as validações ao inserir os nomes dos amigos

---
+++ Projetos_alura_dio/Projeto_ingresso/README.md
@@ -0,0 +1,28 @@
+# Projeto Ingesso: Projeto voltado a compra de Ingressos
+
+* Breve Resumo:
+
+Projeto de javascrip com funcionalidade de compra de ingressos separando por tipo, pista, inferior ou superior decrescendo o valor disponível em estoque.
+
+* Funcionalidades e Demonstração do Site
+
+Tela principal:
+
+![alt text](image.png)
+
+Tela ao lançar compras:
+
+![alt text](image-1.png)
+
+* Tecnologias utilizadas
+
+  * HTML - HyperText Markup Language
+  * CSS - Cascading Style Sheets
+  * JavaScript
+
+* Pessoas Desenvolvedoras do Projeto
+
+  Autores
+
+| [<img loading="lazy" src="https://avatars.githubusercontent.com/u/9308189?v=4" width=115><br><sub>Ericson Nascimento</sub>](https://github.com/ericsonnascimento) |
+| :---: |
---
+++ Projetos_alura_dio/Projeto_ingresso/js/app.js
@@ -0,0 +1,52 @@
+
+function comprar() {
+    let tipo = document.getElementById('tipo-ingresso').value;
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    // Verificar se a quantidade inserida é válida
+    if (isNaN(qtd) || qtd <= 0) {
+        alert("Insira uma quantidade válida.");
+        return;
+    }
+
+    if (tipo == 'pista') {
+        comprarPista(qtd);
+    } else if (tipo == 'superior') {
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponivel para Pista!');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível para tipo superior');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponível para tipo inferior');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra realizada com sucesso!');
+    }
+}
